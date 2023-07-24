86cd84604bfdf1d03b44e374705b7da2d593d2c3
luca.canali <lucacanali.dev@gmail.com>
2023-07-25 00:19:56 +0200

    Create GUI

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import wikipediaapi
+import tkinter
 
 wiki = wikipediaapi.Wikipedia('it')
 #Lista di persone di interesse
@@ -72,7 +73,7 @@ def num_parole(persona):
     else:
         return 0
 
-scores = {}
+'''scores = {}
 #Calcolo punteggi
 for persona in persone:
     score = 0
@@ -86,4 +87,35 @@ for persona in persone:
 #Creazione della classifica ordinando gli elementi in ordine decrescente
 classifica = sorted(scores.items(), key=lambda x: x[1], reverse=True)
 for pos, (persona, score) in enumerate(classifica, 1):
-    print(f"{pos}. {persona}: {score:.2f}")
+    print(f"{pos}. {persona}: {score:.2f}")'''
+
+root = tkinter.Tk()
+root.title("Scoring&Ranking system for Wikipedia pages, by Luca Canali")
+
+input_one = tkinter.Entry()
+input_one.grid(row=0, column=1)
+input_two = tkinter.Entry()
+input_two.grid(row=1, column=1)
+input_three = tkinter.Entry()
+input_three.grid(row=2, column=1)
+input_four = tkinter.Entry()
+input_four.grid(row=3, column=1)
+input_five = tkinter.Entry()
+input_five.grid(row=4, column=1)
+input_six = tkinter.Entry()
+input_six.grid(row=5, column=1)
+input_seven = tkinter.Entry()
+input_seven.grid(row=6, column=1)
+input_eight = tkinter.Entry()
+input_eight.grid(row=7, column=1)
+input_nine = tkinter.Entry()
+input_nine.grid(row=8, column=1)
+input_ten = tkinter.Entry()
+input_ten.grid(row=9, column=1)
+
+button = tkinter.Button(root, text="Generate" ''' command=start()''')
+button.grid(row=10, column=1)
+
+text_one = tkinter.Text()
+text_one.grid(column=1)
+root.mainloop()
